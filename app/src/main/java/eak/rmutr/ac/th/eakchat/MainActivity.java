e48c4f788ca4115a5b9264f95da718225a57fd23
rmutr <sanya.sam@rmutr.ac.th>
2018-01-27 13:41:40 +0700

    MainFragment Success

--- app/src/main/java/eak/rmutr/ac/th/eakchat/MainActivity.java
+++ app/src/main/java/eak/rmutr/ac/th/eakchat/MainActivity.java
@@ -3,11 +3,21 @@ package eak.rmutr.ac.th.eakchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import eak.rmutr.ac.th.eakchat.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/eak/rmutr/ac/th/eakchat/fragment/MainFragment.java
@@ -0,0 +1,29 @@
+package eak.rmutr.ac.th.eakchat.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import eak.rmutr.ac.th.eakchat.R;
+
+/**
+ * Created by Slump on 01/27/2018.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+
+}   // Main Class
