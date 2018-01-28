2bf22b05808af05320534ccdb41d341a4743d2fc
rmutr <sanya.sam@rmutr.ac.th>
2018-01-28 14:18:36 +0700

    Add Chat Fragment Successed

--- app/src/main/java/eak/rmutr/ac/th/eakchat/ServiceActivity.java
+++ app/src/main/java/eak/rmutr/ac/th/eakchat/ServiceActivity.java
@@ -3,12 +3,28 @@ package eak.rmutr.ac.th.eakchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import eak.rmutr.ac.th.eakchat.fragment.ChatFragment;
+import eak.rmutr.ac.th.eakchat.fragment.RegisterFragment;
+
 public class ServiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentServiceFragment, new ChatFragment())
+                    .commit();
+
+        } else {
+
+        }
+
+
 
     }   // Main Method
 
